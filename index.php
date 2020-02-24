9ea98f72cbda9af18e61f4db177b2db660f1c9c5
Isa Maulana Tantra <isamaulanatantra@gmail.com>
2020-02-24 13:41:21 +0700

    membuat file index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Remote Lagi</title>
+</head>
+
+<body>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut laborum ad molestiae quas magnam rerum qui nobis modi a nesciunt cupiditate praesentium quidem aperiam, repudiandae corrupti dolor minima! Id, voluptate?</p>
+</body>
+
+</html>
